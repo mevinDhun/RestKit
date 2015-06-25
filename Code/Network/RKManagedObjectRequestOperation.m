621eafe5ae2be436ecc14a830180bf74db96ec01
Oli Griffiths <oli@tumblr.com>
2015-06-25 11:17:47 -0400

    Reversed RKManagedObjectRequestOperation copyWithZone removal

--- Code/Network/RKManagedObjectRequestOperation.m
+++ Code/Network/RKManagedObjectRequestOperation.m
@@ -889,8 +889,7 @@ BOOL RKDoesArrayOfResponseDescriptorsContainOnlyEntityMappings(NSArray *response
 #pragma mark - NSCopying
 
 - (id)copyWithZone:(NSZone *)zone {
-    RKManagedObjectRequestOperation *operation = [[RKManagedObjectRequestOperation alloc] initWithHTTPRequestOperation:self.HTTPRequestOperation responseDescriptors:self.responseDescriptors];
-//    RKManagedObjectRequestOperation *operation = (RKManagedObjectRequestOperation *)[super copyWithZone:zone]; // FIXME: PTC 20150625
+    RKManagedObjectRequestOperation *operation = (RKManagedObjectRequestOperation *)[super copyWithZone:zone];
     operation.managedObjectContext = self.managedObjectContext;
     operation.managedObjectCache = self.managedObjectCache;
     operation.fetchRequestBlocks = self.fetchRequestBlocks;
